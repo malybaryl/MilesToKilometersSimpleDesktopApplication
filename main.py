45f3cb40ca47621df078c28d701a36d4e1698f4e
Kacper Baryłowicz <112568080+malybaryl@users.noreply.github.com>
2023-12-31 00:59:32 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,74 @@
 import tkinter as tk
 from tkinter import ttk
 import ttkbootstrap as ttk
+from PIL import Image, ImageTk
 
-def convert():
-    mile_input = entry_int.get()
-    kilometers_output = mile_input * 1.61
-    formatted_output = "{:.2f}".format(kilometers_output)+' km'
-    output_string.set(formatted_output)
-
-#window 
-window = ttk.Window(themename = 'journal')
+# window 
+window = ttk.Window(themename='journal')
 window.title("Miles To Kilometers")
 window.geometry('400x200')
 
-#title
-title_lable = ttk.Label(master = window, text='Miles to kilometers', font='Calibri 24 bold')
-title_lable.pack()
+# variables (moved below the window creation)
+convert_miles = tk.BooleanVar(value=True)
+
+# functions
+def convert():
+    mile_input = entry_int.get()
+    if convert_miles.get():
+        kilometers_output = mile_input * 1.61
+        formatted_output = "{:.2f}".format(kilometers_output) + ' km'
+    else:
+        kilometers_output = mile_input / 1.61
+        formatted_output = "{:.2f}".format(kilometers_output) + ' miles'
+    output_string.set(formatted_output)
+
+def change():
+    convert_miles.set(not convert_miles.get())
+    update_title()
+
+def update_title():
+    title_string.set('Miles To Kilometers' if convert_miles.get() else 'Kilometers To Miles')
+
+# title
+title_string = tk.StringVar()
+update_title()  # Setting a starting title
+title_label = ttk.Label(master=window,
+                        textvariable=title_string,
+                        font='Calibri 24 bold')
+title_label.pack()
 
 # input field
-input_frame = ttk.Frame(master = window, )
+input_frame = ttk.Frame(master=window)
 entry_int = tk.IntVar()
-entry = ttk.Entry(master=input_frame, textvariable = entry_int)
-button = ttk.Button(master=input_frame, text= 'convert', command = convert)
-entry.pack(side='left', padx= 10)
-button.pack(side='left') # side = "left" (to entry and button be next to each other) #padx - padding x (px)
-input_frame.pack(pady = 10) #pady - padding y (px) 
+entry = ttk.Entry(master=input_frame, textvariable=entry_int)
+button = ttk.Button(master=input_frame,
+                    text='Convert',
+                    command=convert)
+button_reverse = ttk.Button(master=input_frame, 
+                            text='', 
+                            command=change)
+entry.pack(side='left', 
+           padx=10)
+button.pack(side='left', 
+            padx=10)
+button_reverse.pack(side='left')
+input_frame.pack(pady=10)
 
-#output
+# loading image
+image_path = "img/0.png"
+img = Image.open(image_path)
+img = img.resize((20, 20))
+img = ImageTk.PhotoImage(img)
+button_reverse.config(image=img, 
+                      compound=tk.LEFT)
+
+# output
 output_string = tk.StringVar()
 output_label = ttk.Label(
-    master = window,
+    master=window,
     text='Output',
     font='Calibri 24',
-    textvariable = output_string)
+    textvariable=output_string)
 output_label.pack()
 
 # run
